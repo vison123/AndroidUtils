2daf72d6844a5b8af22b36c418ed1613151c82fe
vison <723855399@qq.com>
2016-10-14 08:40:59 +0800

    feat(): replace app module to androidutils

--- androidutils/src/androidTest/java/com/vison/androidutils/ExampleInstrumentedTest.java
+++ androidutils/src/androidTest/java/com/vison/androidutils/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.vison.androidutils", appContext.getPackageName());
+        assertEquals("com.vison.androidutils.test", appContext.getPackageName());
     }
 }
--- androidutils/src/main/java/com/vison/androidutils/ToastUtil.java
+++ androidutils/src/main/java/com/vison/androidutils/ToastUtil.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.widget.Toast;
 
 /**
- *
  * Created by vison on 2016/10/14.
  */
 
